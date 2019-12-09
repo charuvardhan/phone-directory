d89e5fe2afa049deb54fc36b0e54cfaa8cb57646
Charu <vardhancharu8@gmail.com>
2019-12-09 15:10:17 +0530

    State: Added state in ‘AddSubscriber’ and ‘App’ components

--- src/App.js
+++ src/App.js
@@ -4,46 +4,41 @@ import './App.css';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
+    }
+  }
+
   render() {
-
-    let subscribers = [
-      {
-        id: 1,
-        name: "snoop dogg",
-        phone: "99898989"
-      },
-      {
-        id: 2,
-        name: "nate dogg",
-        phone: "69696969"
-      }
-    ];
-
+    
     return (
       <div>
         <Header heading="Phone Directory" />
         <div className="component-body-container">
           <button className="custom-btn add-btn">Add</button>
+
           <div className="grid-container heading-container">
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
 
           {
-            subscribers.map(sub => {
+            this.state.subscribersListToShow.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
                 <span className="grid-item action-btn-container">
-                <button className="custom-btn delete-btn">Delete</button>
+                  <button className="custom-btn delete-btn">Delete</button>
                 </span>
               </div>
             })
           }
         </div>
-        </div>
- );
-}
+      </div>
+    );
+  }
 }
 
 export default App;
