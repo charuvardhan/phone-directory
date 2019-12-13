61bd18041d85a2e2d4a6c242a92bfa6f866b79d2
Charu <vardhancharu8@gmail.com>
2019-12-14 02:02:18 +0530

    Updates

---
+++ precache-manifest.f1b07025d0b1d7b2ee37f34338e3bc54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c40ce92fa02868e8332ae73d10d7fa",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "6e9cf670cc16e99b1798",
+    "url": "/phone-directory/static/css/main.f891c6a9.chunk.css"
+  },
+  {
+    "revision": "63c06b911abb48c0e949",
+    "url": "/phone-directory/static/js/2.b1aec311.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/phone-directory/static/js/2.b1aec311.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e9cf670cc16e99b1798",
+    "url": "/phone-directory/static/js/main.c1a6b1f4.chunk.js"
+  },
+  {
+    "revision": "a6c5eefbb1e71a21ab91",
+    "url": "/phone-directory/static/js/runtime-main.b8135e7d.js"
+  }
+]);
